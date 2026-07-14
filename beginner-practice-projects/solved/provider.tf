94a5531cc786b10cf263861488b065831ba57139
Anuj Sharma <anujsharmaitpro@gmail.com>
2026-07-14 22:04:19 +0530

    Mask sensitive data in provider.tf
    Updated sensitive information in provider.tf to use placeholders.

--- beginner-practice-projects/solved/provider.tf
+++ beginner-practice-projects/solved/provider.tf
@@ -17,9 +17,9 @@ key_vault {
       recover_soft_deleted_key_vaults = true
     }
   }
-  subscription_id = "64b8e362-61aa-45fb-aef1-420be79135ef"
-  client_id       = "2b6814b3-a9f6-4dc6-81e3-ffc464065a38"
-  client_secret   = "p_r8Q~YlLyUfKqIux~1aX_toL7ord-eXm~w8Rb59"
-  tenant_id       = "18a6ef3e-4273-4c2b-9a4d-7df397096bdd"
+  subscription_id = "xxxxxxxxxxxxxxxxxxxx"
+  client_id       = "xxxxxxxxxxxxxxxxxxxxxx"
+  client_secret   = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+  tenant_id       = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 }
